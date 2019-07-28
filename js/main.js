e83afe27fb6b3e82e2f97e372b74dfab97099d1d
markchang <h03514@gmail.com>
2019-07-28 22:02:43 +0800

    新增JQUERY製作滑動效果

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function($) {
+    $(document).ready(function() {
+
+        $('body').on('click', '#header .about', function() {
+            $('html, body').animate({ scrollTop: $('#about').offset().top }, 1000);
+        })
+        $('body').on('click', '#header .skill', function() {
+            $('html, body').animate({ scrollTop: $('#skill').offset().top }, 1000);
+        })
+        $('body').on('click', '#header .contact', function() {
+            $('html, body').animate({ scrollTop: $('#contact').offset().top }, 1000);
+        })
+
+        $('body').on('click', '#gotop', function() {
+            $('html').animate({ scrollTop: 0 }, 'slow');
+        })
+
+    });
+})($)
