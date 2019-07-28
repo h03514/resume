0ca531f43afc0113b8d804461e60d7f699b09eb8
markchang <h03514@gmail.com>
2019-07-28 20:05:42 +0800

    初版完成, 準備上傳github

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MarkToast</title>
+    <link rel="stylesheet" href="dist/style/reset.css">
+    <link rel="stylesheet" href="dist/style/all.min.css">
+    <link rel="stylesheet" href="dist/style/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/style/main.min.css">
+</head>
+
+<body>
+    <header>
+        <div id="header">
+            <div class="d-flex">
+                <div id="logo" class="mr-auto">MarkToast</div>
+                <nav class="">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Skills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="container">
+            <div id="banner">
+                <div class="pack">
+                    <h1>走在一條前端工程的道路上</h1>
+                    <div class="introduce">
+                        <span>Welcome to my work</span>
+                        <span>Web Design - FrontEnd Development - System Engineer</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div id="about">
+            <div class="pack">
+                <h2>About</h2>
+                <hr>
+                <div class="resume">
+                    <p>你好, 我是<span class="orange">馬克</span>, 也可以叫我<span
+                            class="orange">馬克吐司</span><br>曾經想成為一個專職音樂人卻轉向到了資訊業來的工程師<br>是個前端工程師兼網管工程師<br>我都揶揄說是個<span
+                            class="orange">網路雜工</span>，但依舊每天都樂在其中<br>善於使用框架製作網頁，也有一個全端的團隊<br>舉凡需要機房建置或者網站設計與架設的<br>歡迎跟我聯絡一起洽談合作!
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div id="skill">
+            <div class="pack">
+                <h2>Skill</h2>
+                <hr>
+                <div class="skillTree row">
+                    <div class="col-lg-4">
+                        <i class="fas fa-code"></i>
+                        <div>
+                            <h3>前端工程</h3><br>HTML<br>CSS<br>JAVASCRIPT<br>(Vue、JQuery、BOORSTRAP 3.4)
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <i class="fas fa-server"></i>
+                        <div>
+                            <h3>機房建置</h3><br>雲端系統建置<br>軟硬體系統整合<br>軟硬體設備升級<br>Windows、Linux Server
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <i class="fas fa-network-wired"></i>
+                        <div>
+                            <h3>網路維運</h3><br>網路建置<br>故障排除<br>FireWall、DHCP、DNS
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="contact">
+            <div class="contactContent">
+                <h2>Contact</h2>
+                <hr>
+                <div class="markImg"></div>
+                <div class="name">
+                    <h4>Mark</h4>
+                </div>
+                <div class="email">
+                    <i class="fas fa-envelope-open-text"></i>
+                    <span>h03514@gmail.com</span>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="footerImg">
+            <p class="copyright">Copyright © Mark All rights reserved</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
