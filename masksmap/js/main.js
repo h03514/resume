a70817d64c0d9c003609019c3276a1b2b4731326
markchang <h03514@gmail.com>
2020-03-18 23:24:43 +0800

    新增 身分證尾數判斷可否買口罩

--- masksmap/js/main.js
+++ masksmap/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    let date = new Date();
 
     $('#mapid').height(window.innerHeight);
 
@@ -8,5 +9,33 @@ $(document).ready(function() {
     }
     AjaxJson(ref, ref, masksInfo);
 
+    $('body').on('click', '#checkIdBtn', function() {
+        $('.alertText').remove();
+
+        let html = '';
+        if (!$('#inputText').val()) {
+            return;
+        }
+        if (date.getDay() == 7) {
+            html += '<div class="alertText">今天大家都可以買口罩喔!!</div>';
+            $('#nameText').append(html);
+        }
+        if ($('#inputText').val() % 2 != 0) {
+            html = '';
+            html += '<div class="alertText">你今天<span><strong>可以</strong></span>買口罩唷</div>';
+            $('#nameText').append(html);
+        }
+        if ($('#inputText').val() % 2 == 0) {
+            html = '';
+            html += '<div class="alertText">你今天<span><strong>不可以</strong></span>買口罩唷</div>';
+            $('#nameText').append(html);
+        }
+    });
+
+
+
+
+
     masksInfo('');
+
 });
