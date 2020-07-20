ebae3665f994f2408cb3c0c30f810ba56933bd9a
markchang <h03514@gmail.com>
2020-07-20 08:14:16 +0800

    用Vue製作JK_Store

---
+++ Jhe_Kai/js/main.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+        value: 'ddsew',
+        imgList: [{ name: 'img/1.jpg' }, { name: 'img/2.jpg' }, { name: 'img/3.jpg' }, { name: 'img/4.jpg' }],
+        fluidClass: 'img-fluid',
+    },
+});
