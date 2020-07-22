c1567a12fc84ac05b2f0eac063c226bc9ddb9216
markchang <h03514@gmail.com>
2020-07-22 22:47:58 +0800

    JK完成雛形 RWD尚未作業

--- Jhe_Kai/js/main.js
+++ Jhe_Kai/js/main.js
@@ -1,8 +1,21 @@
-new Vue({
-    el: '#app',
-    data: {
-        value: 'ddsew',
-        imgList: [{ name: 'img/1.jpg' }, { name: 'img/2.jpg' }, { name: 'img/3.jpg' }, { name: 'img/4.jpg' }],
-        fluidClass: 'img-fluid',
-    },
+// new Vue({
+//     el: '#app',
+//     data: {
+//         value: 'ddsew',
+//         imgList: [{ name: 'img/1.jpg' }, { name: 'img/2.jpg' }, { name: 'img/3.jpg' }, { name: 'img/4.jpg' }],
+//         fluidClass: 'img-fluid',
+//     },
+// });
+
+$(document).ready(function() {
+    // let html = '';
+    // for (let i = 1; i < 5; i++) {
+    //     html += `<div class="box box${i}">`;
+    //     html += `<li>`;
+    //     html += `<img src="img/${i}.jpg" alt="" />`;
+    //     html += `<a class="feature-link button-cta-sm has-icon" href="http://www.marktoast.com">Read <i class="fas fa-long-arrow-alt-right"></i></a>`;
+    //     html += `</li>`;
+    //     html += `</div>`;
+    // }
+    // $('main').append(html);
 });
