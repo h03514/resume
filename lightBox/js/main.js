d0317086d23c09773c925987def3de68e25e18e3
Mark <h03514@gmail.com>
2021-04-02 22:00:58 +0800

    版型完成, click 事件尚未完成

--- lightBox/js/main.js
+++ lightBox/js/main.js
@@ -1,39 +1,38 @@
-    let imgAry = [{
-        id: 0,
-        src: '/img/guitar-1245856_1280.jpg'
-    }, {
-        id: 1,
-        src: '/img/guitar-1836655_1280.jpg'
-    }, {
-        id: 2,
-        src: '/img/guitar-2276181_1280.jpg'
-    }];
+let imgAry = [
+  {
+    id: 1,
+    src: "/img/guitar-1245856_1280.jpg",
+  },
+  {
+    id: 2,
+    src: "/img/guitar-1836655_1280.jpg",
+  },
+  {
+    id: 3,
+    src: "/img/guitar-2276181_1280.jpg",
+  },
+];
 
-
-
-
-    window.onload = function() {
-        const vm = Vue.createApp({
-            data() {
-                return {
-                    imgList: imgAry,
-                }
-            },
-
-        })
-        vm.component('my-component', {
-            template: `
+window.onload = function () {
+  const vm = Vue.createApp({
+    data() {
+      return {
+        imgList: imgAry,
+        modal: false,
+      };
+    },
+  });
+  vm.component("my-component", {
+    template: `
             <div class="row">
-            <div>ddwsddd</div>
+                <div>eee</div>
            </div>
-       `
-        })
+       `,
+  });
 
-        // vm.component('xtemplate', {
-        //     template:
-        // })
+  // vm.component('xtemplate', {
+  //     template:
+  // })
 
-        vm.mount('#app');
-
-
-    }
+  vm.mount("#app");
+};
